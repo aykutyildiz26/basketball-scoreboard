465780bf1602cb2895e53212a49dbf112760ae1e
sly <sylvester@archis.company>
2023-10-22 17:48:50 +0100

    scoreboard js review | refactor

--- index.js
+++ index.js
@@ -1,115 +1,102 @@
-let homeTeamScore = document.querySelector("#home-team-score")
-let homeTeamUserInput = document.querySelector("#home-team-user-input")
-let guestTeamScore = document.querySelector("#guest-team-score")
-let guestTeamUserInput = document.querySelector("#guest-team-user-input")
+let homeTeamScore = document.querySelector("#home-team-score");
+let homePointInput = document.querySelector("#home-team-user-input");
+let guestTeamScore = document.querySelector("#guest-team-score");
+let guestPointInput = document.querySelector("#guest-team-user-input");
 
-let homeSumEl = 0
-let guestSumEl = parseInt(0)
+const homeTeamContainer = document.querySelector(".left-container");
+const homeTeamBtns = document.querySelectorAll("#home-team-btn");
+const guestTeamContainer = document.querySelector(".right-container");
+const guestTeamBtns = document.querySelectorAll("#guest-team-btn");
+const entryBtns = document.querySelectorAll(".entry-button");
 
-const homeTeamContainer = document.querySelector(".left-container")
-const homeTeamAddOneBtn = document.querySelector("#home-team-add-one")
-const homeTeamAddtwoBtn = document.querySelector("#home-team-add-two")
-const homeTeamAddthreeBtn = document.querySelector("#home-team-add-three")
-const homeTeamIncreaseBtn = document.querySelector("#home-team-increase")
-const homeTeamDecreaseBtn = document.querySelector("#home-team-decrease")
+let homeSum = 0;
+let guestSum = 0;
 
-const guestTeamContainer = document.querySelector(".right-container")
-const guestTeamAddOneBtn = document.querySelector("#guest-team-add-one")
-const guestTeamAddtwoBtn = document.querySelector("#guest-team-add-two")
-const guestTeamAddthreeBtn = document.querySelector("#guest-team-add-three")
-const guestTeamIncreaseBtn = document.querySelector("#guest-team-increase")
-const guestTeamDecreaseBtn = document.querySelector("#guest-team-decrease")
-
-
-
-updateScores()
-
-
-homeTeamAddOneBtn.addEventListener("click", function(){
-    homeTeamUserInput.value = 1
-})
-homeTeamAddtwoBtn.addEventListener("click", function(){
-    homeTeamUserInput.value = 2
-})
-homeTeamAddthreeBtn.addEventListener("click", function(){
-    homeTeamUserInput.value = 3
-})
-
-guestTeamAddOneBtn.addEventListener("click", function(){
-    guestTeamUserInput.value = 1
-})
-guestTeamAddtwoBtn.addEventListener("click", function(){
-    guestTeamUserInput.value = 2
-})
-guestTeamAddthreeBtn.addEventListener("click", function(){
-    guestTeamUserInput.value = 3
-})
-
-homeTeamIncreaseBtn.addEventListener("click", function(){
-    if(!homeTeamUserInput.value == ""){
-        homeSumEl += parseInt(homeTeamUserInput.value)
-    }
-    if(homeSumEl > 999){
-        homeSumEl = 999
-        updateScores()
-    }
-    else
-    updateScores()
-    compareScores()
-})
-homeTeamDecreaseBtn.addEventListener("click", function(){
-    if(!homeTeamUserInput.value == ""){
-        homeSumEl -= parseInt(homeTeamUserInput.value)
-    }
-    if(homeSumEl <= 0){
-        homeSumEl = 0
-        updateScores()
-    }
-    else
-    updateScores()
-    compareScores()
-})
-
-guestTeamIncreaseBtn.addEventListener("click", function(){
-    if(!guestTeamUserInput.value == ""){
-        guestSumEl += parseInt(guestTeamUserInput.value)
-    }
-    if(guestSumEl > 999){
-        guestSumEl = 999
-        updateScores()
-    }
-    else
-    updateScores()
-    compareScores()
-})
-guestTeamDecreaseBtn.addEventListener("click", function(){
-    if(!guestTeamUserInput.value == ""){
-        guestSumEl -= parseInt(guestTeamUserInput.value)
-    }
-    if(guestSumEl <= 0){
-        guestSumEl = 0
-        updateScores()
-    }
-    else
-    updateScores()
-    compareScores()
-})
-
-function compareScores(){
-    if(homeSumEl > guestSumEl){
-        homeTeamContainer.style.border = "12px solid lime"
-        guestTeamContainer.style.border = "4px solid red"
-    }
-    else if(homeSumEl < guestSumEl){
-        guestTeamContainer.style.border = "12px solid lime"
-        homeTeamContainer.style.border = "4px solid red"
-    }
-    else{
-        homeTeamContainer.style.border = "4px solid whitesmoke"
-        guestTeamContainer.style.border = "4px solid whitesmoke"
-    }
+/**
+ * Updates the home and guest team scores displayed on the page.
+ * Uses the `textContent` property to update the values and converts them to numbers using the `+` operator.
+ */
+function updateScores() {
+  homeTeamScore.textContent = +homeSum;
+  guestTeamScore.textContent = +guestSum;
 }
-function updateScores(){
-    homeTeamScore.textContent = parseInt(homeSumEl)
-    guestTeamScore.textContent = parseInt(guestSumEl) 
+updateScores();
+
+/**
+ * Attaches an event listener to a given score button and sets the value of the associated team input field.
+ * @param {HTMLElement} scoreBtn - The score button to attach the event listener to.
+ * @param {HTMLInputElement} teamInput - The team input field to set the value for.
+ */
+const setScoreValue = (scoreBtn, teamInput) => {
+  scoreBtn.addEventListener("click", function () {
+    teamInput.value = scoreBtn.dataset.score;
+  });
+};
+
+homeTeamBtns.forEach((btn) => setScoreValue(btn, homePointInput));
+guestTeamBtns.forEach((btn) => setScoreValue(btn, guestPointInput));
+
+/**
+ * Compares the home and guest team scores and applies appropriate CSS classes to the team containers.
+ * Adds or removes CSS classes based on the score comparison result.
+ */
+function compareScores() {
+  if (homeSum > guestSum) {
+    homeTeamContainer.classList.remove("badScore", "neutralScore");
+    homeTeamContainer.classList.add("greatScore");
+    guestTeamContainer.classList.remove("greatScore", "neutralScore");
+    guestTeamContainer.classList.add("badScore");
+  } else if (guestSum > homeSum) {
+    guestTeamContainer.classList.remove("badScore", "neutralScore");
+    guestTeamContainer.classList.add("greatScore");
+    homeTeamContainer.classList.remove("greatScore", "neutralScore");
+    homeTeamContainer.classList.add("badScore");
+  } else if (guestSum === homeSum) {
+    homeTeamContainer.classList.remove("badScore", "greatScore");
+    homeTeamContainer.classList.add("neutralScore");
+    guestTeamContainer.classList.remove("badScore", "greatScore");
+    guestTeamContainer.classList.add("neutralScore");
+  }
 }
+
+entryBtns.forEach((btn) => {
+  /**
+   * Executes the corresponding logic based on the button's dataset values.
+   * Updates the scores and applies the score comparison.
+   * @param {Event} event - The button click event.
+   */
+  btn.addEventListener("click", function () {
+    const isHomeBtn = btn.dataset.home === "true" ? true : false;
+    const incScore = btn.dataset.incScore === "true" ? true : false;
+
+    if (isHomeBtn) {
+      // Score Input Validation on Home Team
+      if (incScore) {
+        if (homePointInput.value === "") return;
+        homeSum += +homePointInput.value;
+        updateScores();
+        compareScores();
+      } else {
+        if (homeSum <= 0) return;
+        homeSum -= +homePointInput.value;
+        updateScores();
+        compareScores();
+      }
+    }
+
+    if (!isHomeBtn) {
+      // Score Input Validation on Guest Team
+      if (incScore) {
+        if (guestPointInput.value === "") return;
+        guestSum += +guestPointInput.value;
+        updateScores();
+        compareScores();
+      } else {
+        if (guestSum <= 0) return;
+        guestSum -= +guestPointInput.value;
+        updateScores();
+        compareScores();
+      }
+    }
+  });
+});
